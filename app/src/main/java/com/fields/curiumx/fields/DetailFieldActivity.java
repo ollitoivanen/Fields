79c76d60172dc803c557053a47a1edba2587acd6
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-03-15 17:07:38 +0200

    Working sign in flow woth much more
    You can now check to field and fetch the amount of users. ready to merge.

--- app/src/main/java/com/fields/curiumx/fields/DetailFieldActivity.java
+++ app/src/main/java/com/fields/curiumx/fields/DetailFieldActivity.java
@@ -2,85 +2,143 @@ package com.fields.curiumx.fields;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class DetailFieldActivity extends Activity{
+import static android.content.ContentValues.TAG;
 
-    public static final String PEOPLE_HERE_KEY = "People Here";
-    public static final String FIELD_NAME = "Field Name";
-    public static final String CURRENT_FIELD = "Current Field";
-    public static final String UID = "Uid";
+public class DetailFieldActivity extends Activity {
+
+    public final String PEOPLE_HERE_KEY = "People Here";
+    public final String FIELD_NAME = "Field Name";
+    public final String CURRENT_FIELD = "Current Field";
+    public final String UID = "Uid";
+    public final String PEOPLE_HERE_BY_UID = "People here by uid";
     ImageButton imTrainingHereButton;
-
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-
-    // The details of the venue that is being displayed.
-
-     String venueName;
-    private String venueID;
-    private CollectionReference collectionReference;
+    Button imTrainingHereNoMore;
+    String venueName;
+    String venueID;
+    CollectionReference peopleHereColRef;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     DocumentReference reference;
-
+    String uid = user.getUid();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    TextView amountOfPeople;
+    int size;
 
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_detail);
         getActionBar().setHomeButtonEnabled(true);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         imTrainingHereButton = findViewById(R.id.imTrainingHereButton);
-        imTrainingHereButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onButtonPress();
-            }
-        });
+        imTrainingHereNoMore = findViewById(R.id.imTrainingHereNoMoreButton);
+        imTrainingHereNoMore.setVisibility(View.GONE);
 
+        amountOfPeople = findViewById(R.id.amountOfPeople);
 
-
-
-        // Retrieves venues details from the intent sent from PlacePickerActivity
         Bundle venue = getIntent().getExtras();
-
         venueName = venue.getString("name");
         venueID = venue.getString("ID");
-
         setTitle(venueName);
         TextView fieldName = findViewById(R.id.fieldName);
         fieldName.setText(venueName);
+        peopleHereColRef = db.collection("Fields").document(venueID).collection("People Here");
+        reference = db.collection("Fields").document(venueID);
 
 
-        collectionReference = FirebaseFirestore.getInstance().collection("Fields");
 
 
-//TODO add new field only if it doesn't already exist
-        Map<String, Object> field = new HashMap<>();
-        field.put(FIELD_NAME, venueName);
-        field.put(PEOPLE_HERE_KEY, 0);
-        collectionReference.document(venueID).set(field);
+        peopleHereColRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                                 @Override
+                                                 public void onEvent(QuerySnapshot value, FirebaseFirestoreException e) {
+                                                      Log.d(TAG, "gg" + value.size());
+                                                      amountOfPeople.setText(String.valueOf(value.size()));
 
+                                                 }
+                                             });
+
+
+
+        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (!task.getResult().exists()) {
+                    Map<String, Object> initialData = new HashMap<>();
+                    initialData.put(FIELD_NAME, venueName);
+                    reference.set(initialData);
+
+
+                }
+            }
+        });
+
+        peopleHereColRef.document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.getResult().exists()) {
+                    imTrainingHereNoMore.setVisibility(View.VISIBLE);
+                } else {
+                    imTrainingHereNoMore.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        imTrainingHereButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonPressImHere();
+            }
+        });
+
+        imTrainingHereNoMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonPressImHereNoMore();
+            }
+        });
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -92,15 +150,35 @@ public class DetailFieldActivity extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
-    public void onButtonPress(){
+    public void onButtonPressImHere() {
 
-       String uid = user.getUid();
-       reference = FirebaseFirestore.getInstance().collection("Users").document(uid);
+peopleHereColRef.document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+    @Override
+    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+        if (!task.getResult().exists()){
+            Map<String, Object> data = new HashMap<>();
+            data.put("testing", 0);
+            data.put("name", user.getDisplayName());
+            peopleHereColRef.document(uid).set(data);
+            imTrainingHereNoMore.setVisibility(View.VISIBLE);
+        }else {
+            Toast.makeText(getApplicationContext(), "You are already here", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
+    }
 
-       Map<String, Object> users = new HashMap<>();
-       users.put(CURRENT_FIELD, venueName);
-       users.put(UID, uid);
-       reference.set(users);
-   }
-}
+    public void onButtonPressImHereNoMore(){
+        peopleHereColRef.document(uid).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(getApplicationContext(), "You are not training here anymore", Toast.LENGTH_SHORT).show();
+                imTrainingHereNoMore.setVisibility(View.GONE);
+            }
+        });
+
+    }
+    }
+
+
 
