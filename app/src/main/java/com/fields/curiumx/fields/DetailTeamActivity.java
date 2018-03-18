c0ae33b36dd78df25c450d27457ed892af5a19de
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-03-18 12:40:32 +0200

    Working join button

--- app/src/main/java/com/fields/curiumx/fields/DetailTeamActivity.java
+++ app/src/main/java/com/fields/curiumx/fields/DetailTeamActivity.java
@@ -2,7 +2,16 @@ package com.fields.curiumx.fields;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DetailTeamActivity extends Activity {
 
@@ -10,6 +19,12 @@ public class DetailTeamActivity extends Activity {
     String teamCountry;
     TextView textName;
     TextView textCountry;
+    Button joinButton;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String uid = user.getUid();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,7 @@ public class DetailTeamActivity extends Activity {
 
         textName = findViewById(R.id.teamName);
         textCountry = findViewById(R.id.teamCountry);
+        joinButton = findViewById(R.id.join);
 
 
         Bundle info = getIntent().getExtras();
@@ -26,6 +42,22 @@ public class DetailTeamActivity extends Activity {
 
         textName.setText(teamName);
         textCountry.setText(teamCountry);
+
+        final String username = user.getDisplayName();
+
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> data = new HashMap<>();
+                data.put("User's team", teamName);
+                db.collection("Users").document(uid).set(data);
+
+                Map<String, Object> data1 =new HashMap<>();
+                data1.put("username", username);
+                db.collection("Teams").document(teamName).collection("TeamUsers").document(uid).set(data1);
+            }
+        });
 
     }
 }
