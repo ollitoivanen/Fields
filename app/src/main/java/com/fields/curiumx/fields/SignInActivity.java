0245fdc7c586d9094582984231cb1bc139c30e84
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-03-30 11:51:27 +0300

    Working signup animation
    jeez

--- app/src/main/java/com/fields/curiumx/fields/SignInActivity.java
+++ app/src/main/java/com/fields/curiumx/fields/SignInActivity.java
@@ -7,55 +7,21 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.text.InputType;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-
-import static android.content.ContentValues.TAG;
 
 public class SignInActivity extends Activity implements View.OnClickListener{
 
     FirebaseAuth mAuth;
     EditText emailSignIn, passwordSignIn;
-    GoogleSignInClient mGoogleSignInClient;
-    private final static int RC_SIGN_IN = 3;
-    FirebaseAuth.AuthStateListener mAuthListener;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.addAuthStateListener(mAuthListener);
-
-        if (mAuth.getCurrentUser() != null){
-            finish();
-            startActivity(new Intent(SignInActivity.this, FeedActivity.class));
-        }
-    }
-
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -67,54 +33,22 @@ public class SignInActivity extends Activity implements View.OnClickListener{
         passwordSignIn = findViewById(R.id.passwordSignIn);
         Button signInButton = findViewById(R.id.signInButton);
         TextView signUpTextView = findViewById(R.id.signUpTextView);
-        SignInButton googleSignInButton = findViewById(R.id.googleSignInButton);
-        setGooglePlusButtonText(googleSignInButton, "Sign In with Google account");
         TextView forgotPassword = findViewById(R.id.forgotPassword);
 
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        //Check if user is logged in
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser() != null){
+                if (firebaseAuth.getCurrentUser() != null) {
                     startActivity(new Intent(SignInActivity.this, FeedActivity.class));
                 }
             }
-        };
-
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
         });
-
         signInButton.setOnClickListener(this);
-        signUpTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
-            }
-        });
-
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
-            }
-        });
-
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signUpTextView.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
     }
-
-
-
-
 
     public void userSignIn(){
 
@@ -140,20 +74,20 @@ public class SignInActivity extends Activity implements View.OnClickListener{
         }
 
         if (password.length() < 6) {
-            passwordSignIn.setError("Minimum lenght of password should be 6");
+            passwordSignIn.setError("Minimum length of password should be 6");
             passwordSignIn.requestFocus();
             return;
         }
+
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-
                 if (task.isSuccessful()) {
                     finish();
                     Intent intent = new Intent(SignInActivity.this, FeedActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
-                    Toast.makeText(SignInActivity.this, "Sign in successful", Toast.LENGTH_LONG)
+                    Toast.makeText(SignInActivity.this, "Sign in successful", Toast.LENGTH_SHORT)
                             .show();
 
                 } else {
@@ -163,99 +97,21 @@ public class SignInActivity extends Activity implements View.OnClickListener{
         });
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-            if (requestCode == RC_SIGN_IN) {
-                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                try {
-                    // Google Sign In was successful, authenticate with Firebase
-                    GoogleSignInAccount account = task.getResult(ApiException.class);
-                    firebaseAuthWithGoogle(account);
-                } catch (ApiException e) {
-                    // Google Sign In failed, update UI appropriately
-                    Log.w(TAG, "Google sign in failed", e);
-                    // ...
-                }
-            } else Toast.makeText(SignInActivity.this, "Auth went wrong", Toast.LENGTH_SHORT)
-                    .show();
-
-        }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            final FirebaseUser user = mAuth.getCurrentUser();
-                            final String uid = user.getUid();
-                            db.collection("Users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if (!task.getResult().exists()){
-                                        String displayName = user.getDisplayName().toLowerCase().trim();
-                                        String displayName1 = displayName.replace(" ", "");
-                                        UserMap userMap = new UserMap(displayName1, uid,
-                                                "Not at any field",
-                                                "Not at any field", null, user.getDisplayName());
-
-
-                                        db.collection("Users").document(uid).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                Toast.makeText(SignInActivity.this, "Sign Up successful", Toast.LENGTH_LONG)
-                                                        .show();
-                                            }
-                                        });
-                                    }else{
-                                        Toast.makeText(SignInActivity.this, "Sign In successful", Toast.LENGTH_LONG)
-                                                .show();
-                                    }
-                                }
-                            });
-
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Snackbar.make(findViewById(R.id.signUpActivity), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-
     @Override
     public void onClick(View view) {
 
         switch (view.getId()){
             case R.id.signInButton:
                 userSignIn();
-
                 break;
-        }
-    }
 
-    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
-        // Find the TextView that is inside of the SignInButton and set its text
-        for (int i = 0; i < signInButton.getChildCount(); i++) {
-            View v = signInButton.getChildAt(i);
+            case R.id.signUpTextView:
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+                break;
 
-            if (v instanceof TextView) {
-                TextView tv = (TextView) v;
-                tv.setText(buttonText);
-                return;
-            }
+            case R.id.forgotPassword:
+                startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
+                break;
         }
     }
 }
--- app/src/main/java/com/fields/curiumx/fields/SignUpActivity.java
+++ app/src/main/java/com/fields/curiumx/fields/SignUpActivity.java
@@ -1,90 +1,138 @@
 package com.fields.curiumx.fields;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.support.constraint.ConstraintLayout;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.text.Spanned;
-import android.util.Log;
+import android.transition.TransitionManager;
 import android.util.Patterns;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import static android.content.ContentValues.TAG;
+import org.w3c.dom.Text;
+
+import java.nio.file.Path;
 
 public class SignUpActivity extends Activity implements View.OnClickListener{
 
-    EditText emailSignUp, passwordSignUp, username;
-    String teamName = null;
-
+    EditText emailSignUp, passwordSignUp, username, realName;
+    TextView signInTextView;
+    TextView welcome;
+    Button signUpButton;
     FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    GoogleSignInClient mGoogleSignInClient;
-    private final static int RC_SIGN_IN = 2;
-
-    InputFilter filter = new InputFilter() {
-        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-            String filtered = "";
-            for (int i = start; i < end; i++) {
-                char character = source.charAt(i);
-                if (!Character.isWhitespace(character)) {
-                    filtered += character;
-                }
-            }
-            return filtered;
-        }
-        };
+    ConstraintLayout container1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setTitle("");
 
+
+
+
+
+        //Filters spaces from username
+        InputFilter filter = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                String filtered = "";
+                for (int i = start; i < end; i++) {
+                    char character = source.charAt(i);
+                    if (!Character.isWhitespace(character)) {
+                        filtered += character;
+                    }
+                }
+                return filtered;
+            }
+        };
+        final ImageView logo = findViewById(R.id.logo);
+
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(logo, "y", 1000f, 0f )
+                .setDuration(7000);
+        animator.addListener(new Animator.AnimatorListener(){
+             @Override
+                                         public void onAnimationStart(Animator animator) {
+                 
+                                             logo.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+                                         }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+
+            @Override
+                                         public void onAnimationCancel(Animator animator) {
+                                             logo.setLayerType(View.LAYER_TYPE_NONE, null);
+                                         }
+
+                                         @Override
+                                         public void onAnimationEnd(Animator animator) {
+                                             // See http://stackoverflow.com/a/19615205/321697
+                                             logo.post(new Runnable() {
+                                                 @Override
+                                                 public void run() {
+                                                     logo.setLayerType(View.LAYER_TYPE_NONE, null);
+
+                                                     container1.setVisibility(View.VISIBLE);
+                                                     container1.setAlpha(0.0f);
+                                                     container1.animate().alpha(1.0f).setDuration(2000).setListener(new AnimatorListenerAdapter() {
+                                                         @Override
+                                                         public void onAnimationEnd(Animator animation) {
+                                                             super.onAnimationEnd(animation);
+                                                         }
+                                                     });
+
+
+                                                 }
+                                             });
+                                         }
+                                     });
+
+                animator.start();
+
+
+
+                container1 = findViewById(R.id.container1);
+        welcome = findViewById(R.id.welcome);
+        realName = findViewById(R.id.real_name);
         username = findViewById(R.id.username);
         username.setFilters(new InputFilter[]{filter});
         emailSignUp = findViewById(R.id.emailSignUp);
         emailSignUp.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
         passwordSignUp = findViewById(R.id.passwordSignUp);
-        Button signUpButton = findViewById(R.id.signUpButton);
-        TextView signInTextView = findViewById(R.id.signInTextView);
-        SignInButton googleSignInButton = findViewById(R.id.googleSignIn);
-        setGooglePlusButtonText(googleSignInButton, "Sign up with Google account");
+        signUpButton = findViewById(R.id.signUpButton);
+        signInTextView = findViewById(R.id.signInTextView);
 
-        googleSignInButton.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
         signInTextView.setOnClickListener(this);
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         //Sends user to FeedActivity if user is not null
         mAuth = FirebaseAuth.getInstance();
@@ -100,13 +148,16 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
 
     }
 
-
     public void registerUser() {
         final String emailString = emailSignUp.getText().toString().trim();
         final String passwordString = passwordSignUp.getText().toString().trim();
-        final String usernameString =  username.getText().toString().trim();
+        final String usernameString = username.getText().toString().trim();
+        final String realNameString = realName.getText().toString().trim();
 
-
+        if (realNameString.isEmpty()){
+            realName.setError("Please give real name");
+            realName.requestFocus();
+        }
 
         if (emailString.isEmpty()) {
             emailSignUp.setError("Email is required");
@@ -147,22 +198,31 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT)
+                                Toast.makeText(getApplicationContext(), "Sign up successful", Toast.LENGTH_SHORT)
                                         .show();
 
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 String uid = user.getUid();
 
                                 UserMap userMap = new UserMap(usernameString, uid, "Not at any field",
-                                        "Not at any field", null, null);
+                                        "Not at any field", null, realNameString);
                                 db.collection("Users").document(uid).set(userMap);
-                                startActivity(new Intent(SignUpActivity.this, FeedActivity.class));
+
+                                UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(realNameString)
+                                        .build();
+                                user.updateProfile(profileChangeRequest);
+
+                                Intent intent = new Intent(SignUpActivity.this, FeedActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
 
                             } else {
 
                                 if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                                     Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
-                                    } else {
+                                } else {
                                     Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             }
@@ -171,112 +231,20 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
                     }else{
                     username.setError("This username is taken");
                     username.requestFocus();
-                }
+                    }
             }
         });
-        }
-
-
-    //Google signup stuff
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
-                // ...
-            }
-        } else Toast.makeText(SignUpActivity.this, "Auth went wrong", Toast.LENGTH_SHORT);
-        }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success
-                            Log.d(TAG, "signInWithCredential:success");
-                            final FirebaseUser user = mAuth.getCurrentUser();
-                            final String uid = user.getUid();
-                            db.collection("Users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if (!task.getResult().exists()){
-
-                                        String displayName = user.getDisplayName().toLowerCase().trim();
-                                        String displayName1 = displayName.replace(" ", "");
-                                        UserMap userMap = new UserMap(displayName1, uid,
-                                                "Not at any field",
-                                                "Not at any field", null, user.getDisplayName());
-
-                                        db.collection("Users").document(uid).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                Toast.makeText(SignUpActivity.this, "Sign Up successful", Toast.LENGTH_LONG)
-                                                        .show();
-                                            }
-                                        });
-                                    }else{
-                                        Toast.makeText(SignUpActivity.this, "Sign In successful", Toast.LENGTH_LONG)
-                                                .show();
-                                    }
-                                }
-                            });
-
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Snackbar.make(findViewById(R.id.signUpActivity), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()){
-
             case R.id.signUpButton:
                 registerUser();
-                break;
-
-            case R.id.googleSignIn:
-                signIn();
                 break;
 
             case R.id.signInTextView:
                 startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
         }
     }
-
-    //Set the text of Google sign in button
-    protected void setGooglePlusButtonText(SignInButton signInButton, String buttonText) {
-        // Find the TextView that is inside of the SignInButton and set its text
-        for (int i = 0; i < signInButton.getChildCount(); i++) {
-            View v = signInButton.getChildAt(i);
-
-            if (v instanceof TextView) {
-                TextView tv = (TextView) v;
-                tv.setText(buttonText);
-                return;
-            }
-        }
-    }
-    //Set the text of Google sign in button
 }
